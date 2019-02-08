bea066e03634109db836fc4ae27fc11e8b218ab3
dpeeps <ds016524dp@dsisdconnect.com>
2019-02-07 18:57:03 -0600

    Update SK19Lift.java
    SK19 was updated to include an ArrayList to work as a lookup table. This needs to be changed to be a 2D static array that is intiated at the class level not in the constructor and structured to look like a matrix. The 2D array needs to be of doubles and do a type conversion of doubles to boolean  using 1.0 as false and 2.0 as true.

--- src/main/java/frc/robot/subsystems/SK19Lift.java
+++ src/main/java/frc/robot/subsystems/SK19Lift.java
@@ -1,14 +1,19 @@
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import frc.robot.Ports;
 import frc.robot.subsystems.base.BaseAngleControlledArm;
 import frc.robot.subsystems.base.BasePneumaticElevator;
 import frc.robot.subsystems.base.BaseProximitySensor;
 import frc.robot.utils.ScaledEncoder;
 import frc.robot.subsystems.base.BaseGroveIRProximitySensor;
+import frc.robot.subsystems.base.BaseHatchMechanism;
+import frc.robot.subsystems.base.BaseOctopusRoller;
 
 /**
  *  The SK19Lift subsystem is responsible for both the elevator and arm systems that 
@@ -16,18 +21,32 @@ import frc.robot.subsystems.base.BaseGroveIRProximitySensor;
  */
 public class SK19Lift
 {
-    SpeedController              ArmMotor;
-    BaseAngleControlledArm       RobotArm;
-    BasePneumaticElevator        RobotElevator;
-    BaseGroveIRProximitySensor   ElevatorDownProximitySensor;
-    BaseGroveIRProximitySensor   ElevatorUpProximitySensor;
-    ScaledEncoder                ArmEncoder;
-    BaseProximitySensor          ArmDownLimitSensor;
-    BaseProximitySensor          ArmUpLimitSensor;
-    Solenoid                     ElevatorSolenoid;
+    SpeedController                     ArmMotor;
+    BaseAngleControlledArm              RobotArmAngled;
+    BasePneumaticElevator               RobotElevator;
+    BaseGroveIRProximitySensor          ElevatorDownProximitySensor;
+    BaseGroveIRProximitySensor          ElevatorUpProximitySensor;
+    ScaledEncoder                       ArmEncoder;
+    BaseProximitySensor                 ArmDownLimitSensor;
+    BaseProximitySensor                 ArmUpLimitSensor;
+    Solenoid                            ElevatorSolenoid;
+    BaseOctopusRoller                   OctopusRoller;
+    BaseHatchMechanism                  RobotHatch;
+    DoubleSolenoid                      HatchLockSolenoid;
+    DoubleSolenoid                      HatchDeploySolenoid;
+    BaseProximitySensor                 HatchSensor;
+    BaseProximitySensor                 BallSensor;
 
-    double                       ArmSpeed; 
-    
+    double                              ArmSpeed;
+    private final ArrayList<Boolean>    BoolArrayList;
+    private final ArrayList<Double>    IntHatchArrayList;
+    private final ArrayList<Double>    IntBallArrayList;
+    private final ArrayList<ArrayList>  LookupTable;
+    /**
+     *  Creates the Arm and elevator base classes and seamlessly melds them together so that no matter
+     *  the position that we wish to be at it is a one button click and it automatically goes to the required
+     *  position.
+     */
     public SK19Lift()
     {
         // This is the declarations for the motor controllers, solenoids as well as the arm speed
@@ -36,6 +55,26 @@ public class SK19Lift
         this.ArmSpeed = 1.0;
         this.ArmMotor                    = new WPI_TalonSRX(Ports.armRotateMotor);
         this.ElevatorSolenoid            = new Solenoid(Ports.elevatorPCM);
+        this.HatchDeploySolenoid         = new DoubleSolenoid(Ports.hatchGripperOut, Ports.hatchGripperIn);
+        this.HatchLockSolenoid           = new DoubleSolenoid(Ports.hatchGripperLock, Ports.hatchGripperUnlock);
+        // TODO: This may be converted to a static intiation for a static Array not an array list. This is a Work in Progress and will need to be fixed, come back to first thing on Saturday
+        // To get the answer but it should be converted to double numbers and then use an if statement to get the required boolean out of it.
+        BoolArrayList = new ArrayList<Boolean>();
+        BoolArrayList.add(false);
+        BoolArrayList.add(false);
+        BoolArrayList.add(true);
+        IntBallArrayList = new ArrayList<Double>();
+        IntBallArrayList.add(5.0);
+        IntBallArrayList.add(50.0);
+        IntBallArrayList.add(95.0);
+        IntHatchArrayList = new ArrayList<Double>();
+        IntHatchArrayList.add(0.0);
+        IntHatchArrayList.add(45.0);
+        IntHatchArrayList.add(90.0);
+        LookupTable = new ArrayList<ArrayList>();
+        LookupTable.add(BoolArrayList);
+        LookupTable.add(IntBallArrayList);
+        LookupTable.add(IntHatchArrayList);
 
         // This is the decleration for all of the required sensors
         this.ArmEncoder                  = new ScaledEncoder(Ports.armEncoderA, Ports.armEncoderB, Ports.intakeArmEncoderReverse, Ports.intakeArmEncoderPulsesPerRev, 1.0);
@@ -43,11 +82,13 @@ public class SK19Lift
         this.ArmUpLimitSensor            = new BaseProximitySensor(Ports.armLimitTop);
         this.ElevatorUpProximitySensor   = new BaseGroveIRProximitySensor(Ports.elevatorProximityUp);
         this.ElevatorDownProximitySensor = new BaseGroveIRProximitySensor(Ports.elevatorProximityDown);
-
+        this.HatchSensor                 = new BaseProximitySensor(Ports.hatchContactSwitch);
+        this.BallSensor                  = new BaseProximitySensor(Ports.octopusCargoDetect);
         // This is the decleration for the two base subsytems that we're using, BaseAngledControlledArm
         // As well as BasePneumaticElevator
-        this.RobotArm                    = new BaseAngleControlledArm(this.ArmMotor, ArmEncoder, this.ArmUpLimitSensor, this.ArmDownLimitSensor, this.ArmSpeed);
-        this.RobotElevator               = new BasePneumaticElevator(this.ElevatorSolenoid, this.ElevatorUpProximitySensor, this.ElevatorDownProximitySensor);         
+        this.RobotArmAngled              = new BaseAngleControlledArm(this.ArmMotor, ArmEncoder, this.ArmUpLimitSensor, this.ArmDownLimitSensor, this.ArmSpeed);
+        this.RobotElevator               = new BasePneumaticElevator(this.ElevatorSolenoid, this.ElevatorUpProximitySensor, this.ElevatorDownProximitySensor);     
+        this.RobotHatch                  = new BaseHatchMechanism(this.HatchDeploySolenoid, this.HatchLockSolenoid, this.HatchSensor);
     }
 
     //TODO: The following methods need to be finished, there may be required a 4th, 5th and 6th position
@@ -59,16 +100,33 @@ public class SK19Lift
 
     public void setArmPosition1()
     {
-
+        final ArrayList<Double> PulledDoubleArray;
+        Double PulledDouble;
+        if (this.HatchSensor.getIsTriggered())
+        {   
+            PulledDoubleArray = LookupTable.get(2);
+            PulledDouble = (PulledDoubleArray.get(0));
+            RobotArmAngled.moveToAngle(PulledDouble);
+            RobotElevator.setToDown();
+        }
+        else if (this.BallSensor.getIsTriggered())
+        {
+            PulledDoubleArray = LookupTable.get(1);
+            PulledDouble = PulledDoubleArray.get(0);
+            RobotArmAngled.moveToAngle(PulledDouble);
+            RobotElevator.setToDown();
+        }
     }
 
     public void setArmPosition2()
     {
-
+        RobotArmAngled.moveToAngle(0.0);
+        RobotElevator.setToUp();
     }
 
     public void setArmPosition3()
     {
-
+        RobotArmAngled.moveToAngle(90.0);
+        RobotElevator.setToUp();
     }
 }
