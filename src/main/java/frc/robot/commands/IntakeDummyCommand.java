9b7b77e1089b9b71925e24a159ee191a93350ea6
Dave Wilson <dave@davewilsonphotography.com>
2019-03-16 12:02:49 -0500

    Add a dummy intake command

---
+++ src/main/java/frc/robot/commands/IntakeDummyCommand.java
@@ -0,0 +1,46 @@
+package frc.robot.commands;
+
+import frc.robot.Robot;
+import frc.robot.OI;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * A dummy command which can be excuted merely to cause any existing command on the intake to cancel.
+ */
+public class IntakeDummyCommand extends Command
+{
+    private OI.Mode mode;
+
+    public IntakeDummyCommand(OI.Mode mode)
+    {
+        requires(Robot.Intake);
+        this.mode   = mode;
+    }
+
+    protected void initialize()
+    {
+    }
+
+    protected void execute()
+    {
+        if(mode != Robot.oi.getMode())
+            return;
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished()
+    {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end()
+    {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted()
+    {
+    }
+}
